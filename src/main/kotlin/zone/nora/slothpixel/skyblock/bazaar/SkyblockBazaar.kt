fa01ab011c51c8b0f3e66ce08527d6f7e71cd58b
Nora <ncos@protonmail.ch>
2020-04-30 16:38:21 -0400

    Removed bazaar historic data
    removed from api :(

--- src/main/kotlin/zone/nora/slothpixel/skyblock/bazaar/SkyblockBazaar.kt
+++ src/main/kotlin/zone/nora/slothpixel/skyblock/bazaar/SkyblockBazaar.kt
@@ -1,11 +1,13 @@
 package zone.nora.slothpixel.skyblock.bazaar
 
 import com.google.gson.annotations.SerializedName
-import zone.nora.slothpixel.skyblock.bazaar.historic.SkyblockBazaarWeek
 import zone.nora.slothpixel.skyblock.bazaar.status.SkyblockBazaarQuickStatus
 import zone.nora.slothpixel.skyblock.bazaar.summary.SkyblockBazaarSummary
 
 class SkyblockBazaar {
+    @SerializedName("product_id")
+    val productId = ""
+
     @SerializedName("buy_summary")
     val buySummary = ArrayList<SkyblockBazaarSummary>()
 
@@ -14,7 +16,4 @@ class SkyblockBazaar {
 
     @SerializedName("quick_status")
     val quickStatus = SkyblockBazaarQuickStatus()
-
-    @SerializedName("week_historic")
-    val weekHistoric = ArrayList<SkyblockBazaarWeek>()
 }
